25e6cc2739063ac695348ded5ef75440ca151f12
luca donnaloia <luca.dnnl@gmail.com>
2017-12-27 21:19:51 +0100

    added setTimeout of 20''

--- example.test.js
+++ example.test.js
@@ -1,7 +1,11 @@
 const Nightmare = require('nightmare');
 
 describe('When visiting the homepage', () => {
-  test('using promises', (done) => {
+  beforeAll(() => {
+    jest.setTimeout(20000);
+  });
+
+  test('it should return "Google" as title', (done) => {
     Nightmare({show: false})
       .goto('http://www.google.it')
       .evaluate(() => document.title)
