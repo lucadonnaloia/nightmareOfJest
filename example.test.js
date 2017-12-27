dd6b3be7d3a1812cf73db9449f429f75a21c22f2
luca donnaloia <luca.dnnl@gmail.com>
2017-12-27 19:30:49 +0100

    added done() callback

--- example.test.js
+++ example.test.js
@@ -1,13 +1,25 @@
 const Nightmare = require('nightmare');
 
-describe('When visiting the homepage', function () {
-  test('it should have "Google" as title', async function () {
+describe('When visiting the homepage', () => {
+  test('it should have "Google" as title', done => {
     jest.setTimeout(60000);
     let page = Nightmare({ show: false })
       .goto('http://www.google.it');
-    let text = await page.evaluate(() => document.title)
-      .end();
+    let text = page.evaluate(() => document.title)
+      .end()
+      .then(result => { done(); })
+      .catch(done);
 
     expect(text).toContain('Google');
   });
 });
+
+/* describe('/auth (Login Page)', () => {
+  it('should load without error', done => {
+    nightmare
+      .goto('https://gethoodie.com/auth')
+      .end()
+      .then(result => { done(); })
+      .catch(done);
+  });
+}); */
