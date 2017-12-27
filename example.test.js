b96e6ee6d972353f1118ffa75f145576d009f334
luca donnaloia <luca.dnnl@gmail.com>
2017-12-27 21:36:13 +0100

    refactoring project without circleCI

--- example.test.js
+++ example.test.js
@@ -1,18 +1,13 @@
 const Nightmare = require('nightmare');
 
 describe('When visiting the homepage', () => {
-  beforeAll(() => {
-    jest.setTimeout(20000);
-  });
-
   test('it should return "Google" as title', (done) => {
     Nightmare({show: false})
       .goto('http://www.google.it')
       .evaluate(() => document.title)
       .end()
       .then((title) => {
-        // expect(title).toBe('Google');
-        // console.log(title);
+        expect(title).toBe('Google');
         done();
       });
   });
