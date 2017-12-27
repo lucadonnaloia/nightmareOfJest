34c50eff6107d043577158c5bbf7704bd8ca800c
luca donnaloia <luca.dnnl@gmail.com>
2017-12-27 22:00:38 +0100

    +1 test case

--- example.test.js
+++ example.test.js
@@ -1,16 +1,37 @@
 const Nightmare = require('nightmare');
 
-describe('When visiting the homepage', () => {
+describe('When visiting google homepage', () => {
   beforeAll(() => {
     jest.setTimeout(20000);
   });
-  test('it should return "Google" as title', (done) => {
+  test('should return "Google" as page title', (done) => {
     Nightmare({show: true})
       .goto('http://www.google.it')
       .evaluate(() => document.title)
       .end()
       .then((title) => {
         expect(title).toBe('Google');
+        done();
+      });
+  });
+});
+
+describe('When visiting duckduckgo homepage', () => {
+  beforeAll(() => {
+    jest.setTimeout(20000);
+  });
+  test('should find the nightmare github link first', (done) => {
+    Nightmare({show: true})
+      .goto('https://duckduckgo.com')
+      .type('#search_form_input_homepage', 'github nightmare')
+      .click('#search_button_homepage')
+      .wait('#links_wrapper')
+      .evaluate(() =>
+        document.querySelector('#r1-0 > div > h2 > a.result__a').href
+      )
+      .end()
+      .then((link) => {
+        expect(link).toBe('https://github.com/segmentio/nightmare');
         done();
       });
   });
