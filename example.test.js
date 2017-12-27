29db74bf44da5ed4805d990cb1955516f921bac7
luca donnaloia <luca.dnnl@gmail.com>
2017-12-27 21:11:20 +0100

    fixed code using exit callback

--- example.test.js
+++ example.test.js
@@ -1,25 +1,16 @@
 const Nightmare = require('nightmare');
 
 describe('When visiting the homepage', () => {
-  test('it should have "Google" as title', done => {
-    jest.setTimeout(60000);
-    let page = Nightmare({ show: false })
-      .goto('http://www.google.it');
-    let text = page.evaluate(() => document.title)
+  test('using promises', (done) => {
+    Nightmare({show: false})
+      .goto('http://www.google.it')
+      .wait('body')
+      .evaluate(() => document.title)
       .end()
-      .then(result => { done(); })
-      .catch(done);
-
-    expect(text).toContain('Google');
+      .then((title) => {
+        expect(title).toBe('Google_');
+        console.log(title);
+        done();
+      });
   });
 });
-
-/* describe('/auth (Login Page)', () => {
-  it('should load without error', done => {
-    nightmare
-      .goto('https://gethoodie.com/auth')
-      .end()
-      .then(result => { done(); })
-      .catch(done);
-  });
-}); */
