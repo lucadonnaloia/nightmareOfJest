9ebb9eb8ef27fdcc055a0d476914bd8f093febb2
luca donnaloia <luca.dnnl@gmail.com>
2017-12-27 21:40:26 +0100

    jest timeout set to 20 seconds

--- example.test.js
+++ example.test.js
@@ -1,8 +1,11 @@
 const Nightmare = require('nightmare');
 
 describe('When visiting the homepage', () => {
+  beforeAll(() => {
+    jest.setTimeout(20000);
+  });
   test('it should return "Google" as title', (done) => {
-    Nightmare({show: false})
+    Nightmare({show: true})
       .goto('http://www.google.it')
       .evaluate(() => document.title)
       .end()
