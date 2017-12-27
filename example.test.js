d70324422a9af344871a0adfbb3047a98c54991b
luca donnaloia <luca.dnnl@gmail.com>
2017-12-27 19:12:18 +0100

    Modified jest timeout to 1 minute for CircleCI

--- example.test.js
+++ example.test.js
@@ -2,7 +2,7 @@ const Nightmare = require('nightmare');
 
 describe('When visiting the homepage', function () {
   test('it should have "Google" as title', async function () {
-    jest.useRealTimers();
+    jest.setTimeout(60000);
     let page = Nightmare({ show: true })
       .goto('http://www.google.it');
     let text = await page.evaluate(() => document.title)
