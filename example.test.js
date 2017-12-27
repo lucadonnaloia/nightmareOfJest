bd622dad7988bf5e7efdb3c496078d58cbd20dd6
luca donnaloia <luca.dnnl@gmail.com>
2017-12-27 19:14:39 +0100

    set nightmare show property to false for circleCI

--- example.test.js
+++ example.test.js
@@ -3,7 +3,7 @@ const Nightmare = require('nightmare');
 describe('When visiting the homepage', function () {
   test('it should have "Google" as title', async function () {
     jest.setTimeout(60000);
-    let page = Nightmare({ show: true })
+    let page = Nightmare({ show: false })
       .goto('http://www.google.it');
     let text = await page.evaluate(() => document.title)
       .end();
